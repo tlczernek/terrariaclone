0958e2a3aeabe60ceaf301b08b7ed4356c35c628
kumitsuuu _ <tlczernek@gmail.com>
2026-04-06 18:52:40 +0200

    Window opens and draws

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <raylib.h>
 
 int main()
 {
-	std::cout << "Hello\n";
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(1920, 1080, "Window test");
+
+	while (!WindowShouldClose())
+		{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		
+		
+		DrawRectangle(650, 650, 100, 100, PINK);
+		DrawRectangle(700, 700, 100, 100, { 255, 0, 0, 150 });
+		
+
+		DrawText("Holy shit it's open", 600, 200, 40, RED);
+
+		EndDrawing();
+		}
+
+	CloseWindow();
 
 	return 0;
 }
